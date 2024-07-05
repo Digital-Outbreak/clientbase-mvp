a1c99ed4fb1526fb12ce09e5792ad5b2c589f02d
Shrit1401 <shrit1401@email.com>
2024-07-05 17:39:56 +0530

    adding loom to my page

--- src/components/clients/ClientWelcome.tsx
+++ src/components/clients/ClientWelcome.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import ClientHeader from "./ClientHeader";
+import { oembed } from "@loomhq/loom-embed";
 
 const ClientWelcome = ({ client }: { client: Client }) => {
+  const [videoEmbedHtml, setVideoEmbedHtml] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchLoomVideo = async () => {
+      try {
+        const { html } = await oembed(client.loom, {
+          hideOwner: true,
+          height: 200,
+          gifThumbnail: true,
+        });
+        setVideoEmbedHtml(html);
+      } catch (error) {
+        console.error("Error fetching Loom video:", error);
+      }
+    };
+
+    fetchLoomVideo();
+  }, [client.loom]);
+
   return (
     <div>
       <ClientHeader
@@ -9,8 +30,18 @@ const ClientWelcome = ({ client }: { client: Client }) => {
         companyName={client.companyName}
         active="home"
       />
-      <div className="mt-32">
-        <h1>Welcome {client.name}</h1>
+      <div className="mt-24 ml-3">
+        <h1 className="text-4xl font-bold ">Glad to have you</h1>
+        <p className="mt-1 text-[16px] text-white/55">
+          Kindly Go Over The Video Below To Get Started, also complete the
+          checklist below to get started
+        </p>
+        <div className="mt-5">
+          {videoEmbedHtml && (
+            <div dangerouslySetInnerHTML={{ __html: videoEmbedHtml }} />
+          )}
+        </div>
+        h
       </div>
     </div>
   );
