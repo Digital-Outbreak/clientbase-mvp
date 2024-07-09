029afb1d5c63c744ae705b43779546ee3dfac18d
Shrit1401 <shrit1401@email.com>
2024-07-09 19:03:39 +0530

    backend needed for file manager

--- src/components/clients/ClientUploadFileDialog.tsx
+++ src/components/clients/ClientUploadFileDialog.tsx
@@ -12,6 +12,8 @@ import {
 import { Button } from "../ui/button";
 import UploadForm from "./UploadForm";
 import { useState } from "react";
+import { supabase } from "@/lib/db/db";
+import { showToast } from "@/lib/utils";
 const ClientUploadImageDialog = ({
   children,
   client,
@@ -19,11 +21,33 @@ const ClientUploadImageDialog = ({
   children: React.ReactNode;
   client: Client;
 }) => {
+  const [loading, setLoading] = useState<boolean>(false);
   const [files, setFiles] = useState<File[]>([]);
 
   const uploadImages = async () => {
-    console.log(files);
+    setLoading(true);
+    try {
+      for (const file of files) {
+        const fileName = `${client.companyName}/${client.clientCompany}/${file.name}`;
+        const { error: uploadError } = await supabase.storage
+          .from("file-manager")
+          .upload(fileName, file, {
+            cacheControl: "3600",
+            upsert: true,
+          });
+        if (uploadError) throw uploadError;
+      }
+      setFiles([]);
+      showToast("Files uploaded successfully");
+      if (typeof window !== "undefined") {
+        window.location.reload();
+      }
+    } catch (error: any) {
+      console.error("Error uploading file: ", error.message);
+      showToast("Error uploading file");
+    }
   };
+
   return (
     <Dialog>
       <DialogTrigger>{children}</DialogTrigger>
@@ -39,8 +63,13 @@ const ClientUploadImageDialog = ({
           <DialogClose>
             <Button variant="destructive">Cancel</Button>
           </DialogClose>
-          <Button onClick={uploadImages} className="ml-2" type="button">
-            Upload
+          <Button
+            disabled={files.length === 0 || loading}
+            onClick={uploadImages}
+            className="ml-2"
+            type="button"
+          >
+            {loading ? "Uploading..." : "Upload"}
           </Button>
         </DialogFooter>
       </DialogContent>
--- src/lib/db/db.ts
+++ src/lib/db/db.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from "@prisma/client";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 
 const prismaClientSingleton = () => {
   return new PrismaClient();
@@ -12,3 +13,8 @@ const prisma = globalThis.prisma ?? prismaClientSingleton();
 export default prisma;
 
 if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
+
+export const supabase = createClientComponentClient({
+  supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+});
