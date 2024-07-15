765271413f6bd1a87bca4a54265b9463934d7322
Shrit1401 <shrit1401@email.com>
2024-07-16 00:16:06 +0530

    started with messaging, changed the sidebar

--- src/app/(root)/[agency]/[client]/messages/page.tsx
+++ src/app/(root)/[agency]/[client]/messages/page.tsx
@@ -6,8 +6,83 @@ import { getClientBySlug } from "@/lib/db/client-queries";
 import { useParams } from "next/navigation";
 import React, { useEffect, useState } from "react";
 
+// types.ts
+export interface Chat {
+  id: string;
+  name: string;
+  lastMessage: string;
+  time: string;
+  avatar: string;
+  unread?: number;
+}
+
+export interface Message {
+  id: string;
+  content: string;
+  time: string;
+  sender: "me" | "them";
+}
+
+import { Mic, PaperclipIcon, SearchIcon, Smile } from "lucide-react";
+
+export const chats: Chat[] = [
+  {
+    id: "1",
+    name: "12 A(Maths Group)",
+    lastMessage: "RT1 SET B (2024-25).docx",
+    time: "Yesterday",
+    avatar: "/group-avatar.png",
+  },
+  {
+    id: "2",
+    name: "Shrit Shrivastava (You)",
+    lastMessage: "https://www.instagram.com/reel/C9c-g96Stu3...",
+    time: "Yesterday",
+    avatar: "/user-avatar.png",
+  },
+  {
+    id: "3",
+    name: "7th C 2024-25",
+    lastMessage: "Rashi Ridit: kal",
+    time: "Yesterday",
+    avatar: "/group-avatar.png",
+    unread: 23,
+  },
+  // Add more chat items as needed
+];
+
+export const messages: Message[] = [
+  {
+    id: "1",
+    content: "Activity 3",
+    time: "2:09 pm",
+    sender: "them",
+  },
+  {
+    id: "2",
+    content:
+      "CHILDREN AS DISCUSSED IN CLASS I AM SHARING SET A AND B OF RT HERE SO THAT IF YOU HAVE GOT SET A DURING EXAM ATTEMPT SET B AND VICE VERSA TO BECOME MORE CONFIDENT ON THE CONTENT",
+    time: "8:27 pm",
+    sender: "them",
+  },
+  {
+    id: "3",
+    content: "RT1 SET A (2024-25).pdf",
+    time: "8:27 pm",
+    sender: "them",
+  },
+  {
+    id: "4",
+    content: "RT1 SET B (2024-25).docx",
+    time: "8:27 pm",
+    sender: "them",
+  },
+  // Add more messages as needed
+];
 const MessagesPage = () => {
   const [client, setClient] = useState<Client>();
+  const [selectedChat, setSelectedChat] = useState<Chat | null>(chats[0]);
+
   const params = useParams();
 
   useEffect(() => {
@@ -29,16 +104,112 @@ const MessagesPage = () => {
         </div>
         <div className="flex-1 ml-[6rem] lg:ml-[20%]">
           <div className="pb-8">
-            <div
-              className="flex 
-            flex-col
-            items-center
-            justify-center
-            h-[30rem]
-            "
-            >
-              {/* <ClientProjectManager /> */}
-              <center>🚧🚧 Work In Progress</center>
+            <div className="flex h-screen">
+              {/* Left sidebar */}
+              <div className="w-1/3 bg-white border-r flex flex-col">
+                <div className="p-4 bg-gray-200">
+                  <div className="relative">
+                    <SearchIcon className="absolute left-3 top-3 h-5 w-5 text-gray-400" />
+                    <input
+                      type="text"
+                      placeholder="Search or start new chat"
+                      className="w-full p-2 pl-10 bg-white rounded-lg focus:outline-none"
+                    />
+                  </div>
+                </div>
+                <div className="flex-1 overflow-y-auto">
+                  {chats.map((chat) => (
+                    <div
+                      key={chat.id}
+                      className={`flex items-center p-3 border-b hover:bg-gray-100 cursor-pointer ${
+                        selectedChat?.id === chat.id ? "bg-gray-100" : ""
+                      }`}
+                      onClick={() => setSelectedChat(chat)}
+                    >
+                      <img
+                        src={chat.avatar}
+                        alt="Avatar"
+                        className="w-12 h-12 rounded-full mr-3"
+                      />
+                      <div className="flex-1">
+                        <div className="flex justify-between items-baseline">
+                          <h3 className="font-semibold text-gray-800">
+                            {chat.name}
+                          </h3>
+                          <span className="text-xs text-gray-500">
+                            {chat.time}
+                          </span>
+                        </div>
+                        <p className="text-sm text-gray-600 truncate">
+                          {chat.lastMessage}
+                        </p>
+                      </div>
+                      {chat.unread && (
+                        <span className="bg-green-500 text-white rounded-full px-2 py-1 text-xs ml-2">
+                          {chat.unread}
+                        </span>
+                      )}
+                    </div>
+                  ))}
+                </div>
+              </div>
+
+              {/* Right chat area */}
+              {selectedChat ? (
+                <div className="flex-1 flex flex-col bg-[#efeae2]">
+                  <div className="bg-[#f0f2f5] p-4 flex items-center border-l border-gray-300">
+                    <img
+                      src={selectedChat.avatar}
+                      alt="Avatar"
+                      className="w-10 h-10 rounded-full mr-3"
+                    />
+                    <h2 className="font-semibold text-gray-800">
+                      {selectedChat.name}
+                    </h2>
+                  </div>
+                  <div className="flex-1 overflow-y-auto p-4">
+                    {messages.map((message) => (
+                      <div
+                        key={message.id}
+                        className={`flex ${
+                          message.sender === "me"
+                            ? "justify-end"
+                            : "justify-start"
+                        } mb-4`}
+                      >
+                        <div
+                          className={`${
+                            message.sender === "me"
+                              ? "bg-[#d9fdd3]"
+                              : "bg-white"
+                          } p-3 rounded-lg shadow max-w-xs lg:max-w-md`}
+                        >
+                          <p className="text-gray-800">{message.content}</p>
+                          <div className="text-right text-xs text-gray-500 mt-1">
+                            {message.time}
+                          </div>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                  <div className="bg-[#f0f2f5] p-4 flex items-center">
+                    <Smile className="h-6 w-6 text-gray-600 mr-2 cursor-pointer" />
+                    <PaperclipIcon className="h-6 w-6 text-gray-600 mr-2 cursor-pointer" />
+                    <input
+                      type="text"
+                      placeholder="Type a message"
+                      className="flex-1 p-2 rounded-full focus:outline-none bg-white"
+                    />
+                    <Mic className="h-6 w-6 text-gray-600 ml-2 cursor-pointer" />
+                  </div>
+                </div>
+              ) : (
+                <div className="flex-1 flex items-center justify-center bg-[#f0f2f5]">
+                  <p className="text-xl text-gray-500">
+                    Select a chat to start messaging
+                  </p>
+                </div>
+              )}
             </div>
           </div>
         </div>
--- src/components/owners/OwnerSidebar.tsx
+++ src/components/owners/OwnerSidebar.tsx
@@ -1,75 +1,108 @@
 import React from "react";
-import { Home, FileText, LightbulbOff, Briefcase, Heart } from "lucide-react";
+import {
+  Home,
+  Settings,
+  Users,
+  FileText,
+  LightbulbOff,
+  Briefcase,
+  Heart,
+  CreditCard,
+} from "lucide-react";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
 
 const Sidebar = ({
   owner,
   active,
 }: {
   owner: Owner;
-  active: "home" | "important-links" | "file-manager" | "project-manager";
+  active: "home" | "settings" | "clients";
 }) => {
+  const menuItems = [
+    {
+      id: "home",
+      icon: Home,
+      label: "Home",
+      slug: `
+/${owner.companySlug}
+      `,
+    },
+
+    { id: "clients", icon: Users, label: "Clients", slug: "/clients" },
+  ];
+
+  const teamItems = [
+    {
+      id: "Billing",
+      icon: CreditCard,
+      label: "Billing",
+      slug: `${owner.companySlug}/billing`,
+    },
+    {
+      id: "settings",
+      icon: Settings,
+      label: "Settings",
+      slug: `${owner.companySlug}/settings`,
+    },
+  ];
+
+  const MenuItem = ({
+    id,
+    icon: Icon,
+    label,
+    slug,
+  }: {
+    id: string;
+    icon: React.ElementType;
+    label: string;
+    slug: string;
+  }) => (
+    <li>
+      <Link
+        href={slug}
+        className={cn(
+          "flex items-center p-3 rounded-lg transition-all duration-200 hover:bg-gray-800",
+          active === id && "bg-purple-600 hover:bg-purple-700"
+        )}
+      >
+        <Icon className="mr-0 md:mr-3" size={22} />
+        <span className="font-medium hidden md:inline">{label}</span>
+      </Link>
+    </li>
+  );
+
   return (
-    <div className="w-64 h-screen bg-gray-900 text-white p-6 flex flex-col">
-      <div className="flex items-center mb-10">
-        <div className="w-10 h-10 bg-purple-600 rounded-lg mr-3"></div>
-        <h1 className="text-2xl font-bold">Contacts</h1>
+    <div className="w-20 md:w-64 h-screen bg-gray-900 text-white p-4 md:p-6 flex flex-col">
+      <div className="flex items-center mb-10 justify-center md:justify-start">
+        <div className="w-10 h-10 rounded-lg md:mr-3">
+          <img
+            src={owner.companyIconUrl}
+            alt="Owner Image"
+            className="w-full h-full rounded-lg"
+          />
+        </div>
+        <h1 className="text-2xl font-bold hidden md:inline">
+          {owner.companyName}
+        </h1>
       </div>
 
       <nav className="flex-grow">
         <ul className="space-y-4">
-          <li>
-            <a
-              href="#"
-              className="flex items-center p-3 bg-gray-800 rounded-lg transition-all duration-200 hover:bg-gray-700"
-            >
-              <Home className="mr-3" size={22} />
-              <span className="font-medium">Overview</span>
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="flex items-center p-3 rounded-lg transition-all duration-200 hover:bg-gray-800"
-            >
-              <FileText className="mr-3" size={22} />
-              <span className="font-medium">Tasks</span>
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="flex items-center p-3 rounded-lg transition-all duration-200 hover:bg-gray-800"
-            >
-              <LightbulbOff className="mr-3" size={22} />
-              <span className="font-medium">Insights</span>
-            </a>
-          </li>
+          {menuItems.map((item) => (
+            <MenuItem key={item.id} {...item} />
+          ))}
         </ul>
       </nav>
 
       <div className="mt-auto">
-        <h2 className="text-sm uppercase text-gray-500 mb-3 font-semibold tracking-wider">
-          TEAMS
+        <h2 className="text-sm uppercase text-gray-500 mb-3 font-semibold tracking-wider hidden md:block">
+          Agency Settings
         </h2>
         <ul className="space-y-3">
-          <li>
-            <a
-              href="#"
-              className="flex items-center p-3 rounded-lg transition-all duration-200 hover:bg-gray-800"
-            >
-              <Briefcase className="mr-3" size={22} />
-              <span className="font-medium">Sales</span>
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="flex items-center p-3 rounded-lg transition-all duration-200 hover:bg-gray-800"
-            >
-              <Heart className="mr-3" size={22} />
-              <span className="font-medium">Support</span>
-            </a>
-          </li>
+          {teamItems.map((item) => (
+            <MenuItem key={item.id} {...item} />
+          ))}
         </ul>
       </div>
     </div>
