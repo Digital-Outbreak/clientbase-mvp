79bd357869793f18ed1e71d37d27dcb314fe8c9a
Shrit1401 <shrit1401@email.com>
2024-07-25 01:22:20 +0530

    soft launch #3 for clientbase

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,8 @@ import { NextResponse } from "next/server";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: ["/site", "/api/uploadthing"],
+  publicRoutes: ["/site"],
+  ignoredRoutes: ["/api/uploadthing", "/api/clerk-webhook(/.*)"],
   async afterAuth(auth, req) {
     //rewrite for domains
     const url = req.nextUrl;
